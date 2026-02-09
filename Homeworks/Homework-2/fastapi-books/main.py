22bdbc1bfcdad77ced622e5fed838644e07935f0
Srinidhi Gowda <srinidhi.connects@gmail.com>
2026-02-09 15:51:10 -0800

    Merge Artist Liberty styling into FastAPI book app (Part 1 + Part 2)

--- Homeworks/Homework-2/fastapi-books/main.py
+++ Homeworks/Homework-2/fastapi-books/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from typing import List, Optional
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 BOOKS = []
